b5c2161c41e6c66dae30423445d60eea4729132f
luhverchikv <luhverchikv@icloud.com>
2026-03-19 15:28:41 +0300

    Update encryption.py

--- utils/encryption.py
+++ utils/encryption.py
@@ -1,1 +1,19 @@
-#
+# utils/encryption.py
+from cryptography.fernet import Fernet
+from config import config
+
+
+# Используем ключ из конфига
+fernet = Fernet(config.encryption.fernet_key.encode())
+
+
+def encrypt_text(text: str) -> str:
+    return fernet.encrypt(text.encode()).decode()
+
+
+def decrypt_text(token: str) -> str:
+    return fernet.decrypt(token.encode()).decode()
+
+# Генерация нового ключа
+#key = Fernet.generate_key()
+#print("Ваш ключ:", key.decode())
